5cadf565a086863657080148dfb047f0561037dc
Mauro Pereira <mauropereira1807@gmail.com>
2026-02-26 12:29:44 -0100

    adicionar mais funcoes servico

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,61 @@
-import express from "express"
+import express, { type Request, type Response } from "express";
+import {adicionarServico, ApagarServico, ListarServicos, ObterServico} from "./servico.js"
 
 const app = express()
+app.use(express.json())
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     console.log("Hello World!")
     res.send("Hello World!")
 });
+
+
+// rota para adicionar servicos
+app.post("/adicionar-servico", (req: Request, res: Response) => {
+    const novoServico = req.body
+
+    console.log({serv: novoServico})
+    
+    const AddServicoresponse = adicionarServico(novoServico)
+
+    res.json(AddServicoresponse)
+}
+)
+
+// rota para listar os servicos
+app.get("/listar-servico", (req: Request, res: Response) => {
+    const listServicoResponse = ListarServicos()
+
+    res.json(listServicoResponse)
+})
+
+// rota para apagar os servicos
+app.delete("/apagar-servico", (req: Request, res: Response) => {
+    const { nome } = req.query
+    if (nome) {
+        const ApagarServicoResponse = ApagarServico(nome as string)
+        res.json(ApagarServicoResponse)
+    } else {
+        res.json({
+                        message: "Nome do serviço obrigatorio"
+        }
+        )
+    }
+})
+
+// rota para obter servico pelo nome
+app.get("/obter-servico", (req: Request, res: Response) => {
+    const { nome } = req.query
+    if (nome) {
+        const ObterServicoResponse = ObterServico(nome as string)
+        res.json(ObterServicoResponse)
+    } else {
+        res.json({
+                        message: "Nome do serviço obrigatorio"
+        }
+        )
+    }
+})
 
 app.listen(8080, () =>
     console.log("Server running on port 8080")
--- src/servico.ts
+++ src/servico.ts
@@ -8,21 +8,68 @@ interface ServicoType {
     percentagemDesconto: number
 }
 
+interface ResponseType {
+    status: boolean
+    nomeServico: string
+    data: ServicoType
+}
+
 let CategoriaServicos: Array<ServicoType> = []
 
-export  function adicionarServico(servico: ServicoType) {
+// adicionar um serviço novo
+export function adicionarServico(servico: ServicoType) {
+    console.log(servico)
     if (!servico.nome || servico.precoHora <= 0) {
+        console.log("Data received:", CategoriaServicos);
+        // Check if this is undefined!
+
         return "Erro: nome e preço obrigatorio"
     }
-    for (let s of CategoriaServicos)
-        if (s.nome === servico.nome) {
+    for (let s of CategoriaServicos) {
+        if (s?.nome === servico.nome) {
             return console.log("Serviço adicionado")
         }
-        CategoriaServicos.push(servico)
-        console.log("Serviço adicionado")
+    }
+    CategoriaServicos.push(servico)
+    console.log("Serviço adicionado")
     return ({
-        nome: servico.nome,
-        precoHora: servico.precoHora
+        status: true,
+        nomeServico: servico.nome,
+        data: servico
+
     })
 }
 
+// Listar todos os serviços
+export function ListarServicos(): ServicoType[] {
+    // TODO: implementar fetch de servicos
+
+    return CategoriaServicos
+}
+
+// apagar um servico
+export function ApagarServico(nome: string): boolean {
+    // TODO: implementar delete de servico 
+
+    const novoCatalogoTemp: ServicoType[] = []
+
+
+    for (let i = 0; i < CategoriaServicos.length; i++) {
+        if (CategoriaServicos[i]?.nome !== undefined && CategoriaServicos[i]?.nome !== nome) {
+            novoCatalogoTemp.push(CategoriaServicos[i]!)
+        }
+    } // devoçve um novo catalogo sem o item removido
+
+    CategoriaServicos = novoCatalogoTemp
+    return true
+}
+
+// obter um novo servico
+export function ObterServico(nome: string): ServicoType | null {
+    for (let i = 0; i < CategoriaServicos.length; i++) {
+        if (CategoriaServicos[i]?.nome === nome) {
+            return CategoriaServicos[i]!
+        }
+    }
+    return null
+}
