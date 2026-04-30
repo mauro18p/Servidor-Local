ca1f1f1e89600cb3a7688f68b1eb5e5af95110ba
Mauro Pereira <mauropereira1807@gmail.com>
2026-04-30 09:22:21 -0100

    Final adjustments to orcamento controller

--- servidor-local-main/src/controllers/orcamento.controller.ts
+++ servidor-local-main/src/controllers/orcamento.controller.ts
@@ -1,8 +1,10 @@
 import type { Request, Response } from "express"
-import type { OrcamentoDBType, PrestacaoServicoDBType, ResponseType } from "../utils/types.js"
+import type { OrcamentoDBType, PrestacaoServicoDBType, PropostaDBType, ResponseType } from "../utils/types.js"
 import { OrcamentoModel } from "../models/oramento.model.js"
 import { PrestacaoServicoModel } from "../models/prestacao-servico.models.js"
 import { FreelancerModel } from "../models/prestador.model.js"
+import { PropostaModel } from "../models/proposta.model.js"
+import { id } from "date-fns/locale"
 
 
 export const OrcamentoController = {
@@ -67,7 +69,7 @@ export const OrcamentoController = {
                 status: "error",
                 message: "ID obrigatorio",
                 data: null
-        }
+            }
             return res.status(400).json(response)
         }
 
@@ -78,7 +80,7 @@ export const OrcamentoController = {
                 status: "error",
                 message: "Orcamento nao encontrado",
                 data: null
-        }
+            }
             return res.status(404).json(response)
         }
 
@@ -142,7 +144,7 @@ export const OrcamentoController = {
                 status: "error",
                 message: "ID obrigatorio",
                 data: null
-        }
+            }
             return res.status(400).json(response)
         }
 
@@ -153,7 +155,7 @@ export const OrcamentoController = {
                 status: "error",
                 message: "Erro ao apagar orcamento",
                 data: null
-        }
+            }
             return res.status(404).json(response)
         }
 
@@ -168,14 +170,14 @@ export const OrcamentoController = {
     },
 
     async calcular(req: Request, res: Response) {
-    const { id } = req.params;
+        const { id } = req.params;
 
         if (!id) {
             const response: ResponseType<OrcamentoDBType> = {
                 status: "error",
                 message: "ID obrigatorio",
                 data: null
-        }
+            }
             return res.status(400).json(response)
         }
 
@@ -184,11 +186,11 @@ export const OrcamentoController = {
 
 
         if (!prestacoes || prestacoes.length === 0) {
-             return res.status(404).json({
-                 status: "error",
-                 message: "Nenhuma prestação de serviço ativa associada a este orçamento.",
-                 data: null
-             });
+            return res.status(404).json({
+                status: "error",
+                message: "Nenhuma prestação de serviço ativa associada a este orçamento.",
+                data: null
+            });
         }
 
         let totalGeral = 0;
@@ -196,18 +198,18 @@ export const OrcamentoController = {
         for (const prestacao of prestacoes) {
             let custoBase = prestacao.horas_estimadas * prestacao.preco_hora;
             let desconto = 0
-            let urgencia = 0 
+            let urgencia = 0
 
             const prestador = await FreelancerModel.get(prestacao.id_prestador) as any;
 
             if (prestador) {
-                 if (prestacao.urgente === true) {
-                     urgencia = custoBase * prestador.taxa_urgencia;
-                 }
+                if (prestacao.urgente === true) {
+                    urgencia = custoBase * prestador.taxa_urgencia;
+                }
 
-                 if (custoBase >= prestador.minimo_desconto) {
+                if (custoBase >= prestador.minimo_desconto) {
                     desconto = custoBase * (prestador.percentagem_desconto / 100);
-                 }
+                }
             }
 
             totalGeral += custoBase - desconto + urgencia;
@@ -217,99 +219,99 @@ export const OrcamentoController = {
 
         if (!updateResult) {
             return res.status(500).json({
-                 status: "error",
-                 message: "Ocorreu um erro ao gravar o valor absoluto no banco de dados.",
-                 data: null
-            });
-        }
-
-        return res.status(200).json({
-             status: "success",
-             message: "Orçamento calculado e atualizado com sucesso.",
-             data: { id_orcamento: id, total_calculado: parseFloat(totalGeral.toFixed(2)) }
-        });
-    }
-
-    
-}
-
-        const prestacaoServico = await PrestacaoServicoModel.getByIdOrcamento(
-            id as string,
-        );
-
-        if (!prestacaoServico) {
-            return res.status(404).json({
                 status: "error",
-                message: "Prestacao de servico nao encontrado",
-                data: null,
-            });
-        }
-
-        const proposals = await PropostaModel.getByIdPrestacaoServico(
-            prestacaoServico.id,
-        );
-
-        if (!proposals) {
-            return res.status(404).json({
-                status: "error",
-                message: "Ainda nenhuma proposta foi aceite",
-                data: null,
-            });
-        }
-
-        const acceptedProposal: PropostaDBType | undefined = proposals.find((proposal) => proposal.estado === "aceite",
-        );
-
-        if (!acceptedProposal) {
-            return res.status(404).json({
-                status: "error",
-                message: "Ainda nenhuma proposta foi aceite",
-                data: null,
-            });
-        }
-
-        const precoHora = acceptedProposal.preco_hora;
-        const horasEstimadas = prestacaoServico.horas_estimadas;
-
-        const prestador = await FreelancerModel.get(acceptedProposal.id_prestador); // create id_prestador em PropostaDBType
-
-        if (!prestador) {
-            return res.status(404).json({
-                status: "error",
-                message: "Prestador nao encontrado",
-                data: null,
-            });
-        }
-
-        const urgencyTax = prestador.taxa_urgencia;
-        const minimoDesconto = prestador.minimo_desconto;
-        const discountPercentage = prestador.percentagem_desconto;
-
-        let subtotal = precoHora * horasEstimadas;
-
-        if (subtotal >= minimoDesconto) {
-            subtotal = subtotal * (1 - discountPercentage);
-        }
-
-        if (prestacaoServico.urgente) {
-            subtotal = subtotal * (1 - urgencyTax)
-        }
-
-        const updatePrestacaoServicoResponse = await OrcamentoModel.updateBudget(id as string, subtotal);
-
-        if (!updatePrestacaoServicoResponse) {
-            return res.status(500).json({
-                status: "error",
-                message: "Erro ao atualizar budget",
-                data: null,
+                message: "Ocorreu um erro ao gravar o valor absoluto no banco de dados.",
+                data: null
             });
         }
 
         return res.status(200).json({
             status: "success",
-            message: "Budget calculado com sucesso",
-            data: updatePrestacaoServicoResponse,
+            message: "Orçamento calculado e atualizado com sucesso.",
+            data: { id_orcamento: id, total_calculado: parseFloat(totalGeral.toFixed(2)) }
         });
+    }
+
+
+}
+
+const prestacaoServico = await PrestacaoServicoModel.getByIdOrcamento(
+    id as string,
+);
+
+if (!prestacaoServico) {
+    return res.status(404).json({
+        status: "error",
+        message: "Prestacao de servico nao encontrado",
+        data: null,
+    });
+}
+
+const proposals = await PropostaModel.getByIdPrestacaoServico(
+    prestacaoServico.id,
+);
+
+if (!proposals) {
+    return res.status(404).json({
+        status: "error",
+        message: "Ainda nenhuma proposta foi aceite",
+        data: null,
+    });
+}
+
+const acceptedProposal: PropostaDBType | undefined = proposals.find((proposal) => proposal.estado === "aceite",
+);
+
+if (!acceptedProposal) {
+    return res.status(404).json({
+        status: "error",
+        message: "Ainda nenhuma proposta foi aceite",
+        data: null,
+    });
+}
+
+const precoHora = acceptedProposal.preco_hora;
+const horasEstimadas = prestacaoServico.horas_estimadas;
+
+const prestador = await FreelancerModel.get(acceptedProposal.id_prestador); // create id_prestador em PropostaDBType
+
+if (!prestador) {
+    return res.status(404).json({
+        status: "error",
+        message: "Prestador nao encontrado",
+        data: null,
+    });
+}
+
+const urgencyTax = prestador.taxa_urgencia;
+const minimoDesconto = prestador.minimo_desconto;
+const discountPercentage = prestador.percentagem_desconto;
+
+let subtotal = precoHora * horasEstimadas;
+
+if (subtotal >= minimoDesconto) {
+    subtotal = subtotal * (1 - discountPercentage);
+}
+
+if (prestacaoServico.urgente) {
+    subtotal = subtotal * (1 - urgencyTax)
+}
+
+const updatePrestacaoServicoResponse = await OrcamentoModel.atualizarTotal(id as string, subtotal);
+
+if (!updatePrestacaoServicoResponse) {
+    return res.status(500).json({
+        status: "error",
+        message: "Erro ao atualizar budget",
+        data: null,
+    });
+}
+
+return res.status(200).json({
+    status: "success",
+    message: "Budget calculado com sucesso",
+    data: updatePrestacaoServicoResponse,
+});
 
     }
 };
